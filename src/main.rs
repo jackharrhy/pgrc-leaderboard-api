2925e0ffec05608dcb64abc7ea0d0ee6796846d7
Jack Arthur Harrhy <me@jackharrhy.com>
2024-10-29 15:20:11 -0230

    turns out its just {CLASS}_{INTERNAL TRACK NAME}
    the game stdouts when i ran it via the terminal all this, nice

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-use steamworks::{Client,AppId};
+use steamworks::{Client, AppId, LeaderboardDataRequest};
 
 fn main() {
     let (client,single) = Client::init_app(AppId(2737300)).unwrap();
 
     let stats = client.user_stats();
 
-    stats.find_leaderboard("???", |lb| {
+    let client_clone = client.clone();
+
+    stats.find_leaderboard("ultra_chicago", move |lb| {
         println!("Got: {:?}", lb);
+
+        let lb = lb.unwrap().unwrap();
+
+        let stats = client_clone.user_stats();
+
+        println!("about dl entries pls");
+
+        stats.download_leaderboard_entries(
+            &lb,
+            LeaderboardDataRequest::Global,
+            0,
+            10,
+            10,
+            |v| {
+                println!("Download: {:?}", v);
+            }
+        );
     });
 
     for _ in 0..50 {
